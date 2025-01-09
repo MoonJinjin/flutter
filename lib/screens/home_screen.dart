792d4333cd899f4e71746a0a4a11f2f4c8592709
MoonJinjin <moondal1020@gmail.com>
2025-01-09 22:44:54 +0900

    API 응답을 클래스 객체로 변환

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_services.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> wentoons = [];
+  bool isLoading = true;
+
+  void waitForWebToons() async {
+    wentoons = await ApiServices.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(wentoons);
+    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,16 +1,28 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiServices {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
-  void getTodaysToons() async {
+  // static : 해당 함수를 호출하는곳마다 ApiServices 객체를 생성하지 않고 사용하기 위함
+  // static을 사용하지 않으면 Apiservice().getTodaysToons()로 클래스 객체를 만든 후 사용하여야함
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    // async 함수일때는 void 또는 Future 객체로 리턴
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        // json 응답을 클래스 객체로 변환하여 리스트 추가
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
